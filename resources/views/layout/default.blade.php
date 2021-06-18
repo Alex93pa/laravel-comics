f4efacd0556e488459583f9d74587e9dab4baa72
Alex93pa <Ale.pug2@gmail.com>
2021-06-18 17:12:15 +0200

    scaffolding

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -8,7 +8,7 @@
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
   @yield('header_scripts')
 </head>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="nav-up">
+
+        <div>
+            <h5>DC POWER VISA</h5>
+        </div>
+        <div>
+            <h5> ADDITIONAL DC SITES</h5>
+        </div>
+    </div>
+
+    <nav>
+        <div class="logo">
+            <img src="public\images\dc-logo.png" alt="">
+        </div>
+        <div class="nav-list">
+            <ul>
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+</header>
